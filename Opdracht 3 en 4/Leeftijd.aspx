582834db4e348a3b17339dc9bab209e66d715cd1
Maico Gieteling <maicogieteling@gmail.com>
2017-03-14 10:46:47 +0100

    Add files via upload
    Versie 1.0

---
+++ Opdracht 3 en 4/Leeftijd.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Leeftijd.aspx.cs" Inherits="Opdracht_3_en_4.Leeftijd" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="kalender">
+    <asp:Calendar ID="kalVerjaardag" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" VisibleDate="1995-01-01"></asp:Calendar>
+        <br />
+        U heeft de onderstaande datum geselecteerd:<br />
+        <br />
+        <asp:Label style="color:blue;" ID="Label1" runat="server">Selecteer een datum</asp:Label>
+        <br />
+        <br />
+        U bent nu:<br />
+        <br />
+        <asp:Label style="color:blue;" ID="Label2" runat="server">Druk op &quot;Laat zien&quot;</asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnLeeftijd" runat="server" OnClick="btnLeeftijd_Click" Text="Laat zien" />
+    </div>
+
+    <br />
+
+    <div id="countdown">
+        <h1>Het aftellen is begonnen</h1>
+        Het is vandaag:
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;
+        <asp:Literal ID="litVandaag" runat="server"></asp:Literal>
+        <br />
+        <br />
+        Het is nu:<br />
+        <br />
+        &nbsp;&nbsp;&nbsp;
+        <asp:Literal ID="litTijd" runat="server"></asp:Literal>
+        <br />
+        <h3>Wanneer is het Pasen?</h3>
+        <br />
+        Het duurt nog:<br />
+        <br />
+        <asp:Label ID="lblUren" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lblMin" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lblSec" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnVervers" runat="server" Text="Verversen" />
+        <br />
+
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ Opdracht 3 en 4/Leeftijd.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Opdracht_3_en_4
+{
+    public partial class Leeftijd : System.Web.UI.Page
+    {
+        int dagPasen = 106;
+        int dagen;
+        int uren;
+        int minuten;
+        int seconden;
+
+        public static int GetDaysInYear(int year)
+        {
+            DateTime thisYear = new DateTime(year, 1, 1);
+            DateTime nextYear = new DateTime(year + 1, 1, 1);
+
+            return (nextYear - thisYear).Days;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime vandaag = DateTime.Now;
+            DateTime pasen = new DateTime(2017, 4, 16, 0, 0, 0);
+            TimeSpan periode = pasen - vandaag;
+
+            litVandaag.Text = DateTime.Now.Date.ToString("dd-MM-yyyy");
+            litTijd.Text = DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss");
+            lblUren.Text = "" + Convert.ToInt32(periode.TotalHours) + " uren";
+            lblMin.Text = "" + Convert.ToInt32(periode.TotalMinutes) + " minuten";
+            lblSec.Text = "" + Convert.ToInt32(periode.TotalSeconds) + " seconden";
+            if (DateTime.Now.DayOfYear == dagPasen || DateTime.Now.DayOfYear == dagPasen + 1)
+            {
+                lblUren.Text = "Vandaag is het pasen";
+                lblMin.Text = "";
+                lblSec.Text = "";
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label1.Text = kalVerjaardag.SelectedDate.ToString("dd-MM-yyyy");
+        }
+
+        protected void btnLeeftijd_Click(object sender, EventArgs e)
+        {
+            if (kalVerjaardag.SelectedDate == DateTime.MinValue.Date)
+                return;
+
+            int age = DateTime.Now.Year - kalVerjaardag.SelectedDate.Year;
+
+            if (DateTime.Now.Month < kalVerjaardag.SelectedDate.Month || (DateTime.Now.Month == kalVerjaardag.SelectedDate.Month && DateTime.Now.Day < kalVerjaardag.SelectedDate.Day))
+                age--;
+
+            Label2.Text = "" + age;
+        }
+    }
+}
